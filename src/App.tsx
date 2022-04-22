cb6a62edfe524ecc70f81fb1dfaf26bdd93d4b99
liguoyuan819 <2550648956@qq.com>
2022-04-22 18:00:21 +0800

    石头剪刀布小游戏提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Stone from './components/games_01/Stone'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       {<Stone/>}
