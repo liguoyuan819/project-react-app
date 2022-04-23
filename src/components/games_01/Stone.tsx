2e427d696545ae3a9067e24a3241878c7b349eed
liguoyuan819 <2550648956@qq.com>
2022-04-23 13:17:38 +0800

    页面更新数据改成setState

--- src/components/games_01/Stone.tsx
+++ src/components/games_01/Stone.tsx
@@ -3,6 +3,12 @@ import './stone.css'
 import { GAME_TYPE } from './stoneGame'
 
 export default class Stone extends Component{
+    state = {
+        playerScore:0,
+        computerScore:0,
+        rockText:'石头',
+        computerText:'剪刀'
+    };
     choices = [
         {
             id:1,
@@ -22,88 +28,70 @@ export default class Stone extends Component{
     ];
     randomNumber: number  = 0;
     playerImgTag: any;
-    playerRockTag : any;
     computerImgTag : any;
-    computerTextTag : any;
-    points = [0,0];
-    playerPoints: any;
-    computerPoints: any;
     playerImg = (element: any) => {
         this.playerImgTag = element;
    };
-   rockText = (element: any) => {
-        this.playerRockTag = element;
-   }
 
    computerImg = (element: any) => {
         this.computerImgTag = element;
    }
 
-   computerText= (element: any) => {
-        this.computerTextTag = element;
-   }
-
-   playerPointsRef = (element: any) => {
-        this.playerPoints = element;
-   }
-
-   computerPointsRef = (element: any) => {
-        this.computerPoints = element;
-   }
 
     personChoice = (gameType:Number) => {
         return () => {
             this.playerImgTag.src = this.choices.filter(item => item.id === gameType)[0].img;
-            this.playerRockTag.innerHTML = this.choices.filter(item => item.id === gameType)[0].name;
+            this.setState({rockText:this.choices.filter(item => item.id === gameType)[0].name});
             this.getComputerChoice();
         };
     }
 
     getComputerChoice = () => {
         this.computerImgTag.src = '/assets/img/gif.gif';
-        this.computerTextTag.innerHTML = '';
+        this.setState({computerText:''});
         setTimeout(() => {
             this.randomNumber = Math.floor(Math.random() * 3);
             this.computerImgTag.src = this.choices[this.randomNumber].img;
-            this.computerTextTag.innerHTML = this.choices[this.randomNumber].name;
-            this.gameRules();
-            this.playerPoints.innerHTML = this.points[0];
-            this.computerPoints.innerHTML = this.points[1];
+            this.setState({computerText:this.choices[this.randomNumber].name},() => {
+                this.gameRules();
+            });
         },2000);
     }; 
 
     gameRules = () => {
-        if(this.playerRockTag.innerHTML === this.choices[0].name && this.computerTextTag.innerHTML === this.choices[1].name){
-            this.points[0]++;
-        } else if(this.playerRockTag.innerHTML === this.choices[1].name && this.computerTextTag.innerHTML === this.choices[2].name){
-            this.points[0]++;
-        } else if(this.playerRockTag.innerHTML === this.choices[2].name && this.computerTextTag.innerHTML === this.choices[0].name){
-            this.points[0]++;
-        } else if(this.playerRockTag.innerHTML === this.computerTextTag.innerHTML) {
-            
+        let {playerScore, computerScore,rockText,computerText} = this.state;
+        if(rockText === this.choices[0].name && computerText === this.choices[1].name){
+            this.setState({playerScore:playerScore+1});
+        } else if(rockText === this.choices[1].name && computerText === this.choices[2].name){
+            this.setState({playerScore:playerScore+1});
+        } else if(rockText === this.choices[2].name && computerText === this.choices[0].name){
+            this.setState({playerScore:playerScore+1});
+        } else if(rockText === computerText) {
+            this.setState({...this.state});
         } else {
-            this.points[1]++;
+            this.setState({computerScore:computerScore+1});
         }
     }
 
     render(){
+        const {playerScore,computerScore,rockText,computerText} = this.state;
         return <div id="container">
             <h2>石头剪刀布</h2>
             <div id="game-container">
                 <div id="points">
                     <label htmlFor="playerPoints">玩家分数：</label>
-                    <span className="playerPoints" ref={this.playerPointsRef}>{this.points[0]}</span>
+                    <span className="playerPoints">{playerScore}</span>
                     <label htmlFor="computerPoints">电脑分数：</label>
-                    <span className="computerPoints" ref={this.computerPointsRef}>{this.points[1]}</span>
+                    <span className="computerPoints">{computerScore}</span>
                 </div>
                 <div id="choice">
                     <div id="playerChoice">
                     <img ref={this.playerImg} src="/assets/img/rock.png" alt="" id="playerChoiceImg"/>
-                    <h3 id="playerChoiceTxt" ref={this.rockText}>石头</h3>
+                    <h3 id="playerChoiceTxt">{rockText}</h3>
                     </div>
                     <div id="computerChoice">
                     <img ref={this.computerImg} src="/assets/img/scissors.png" alt="" id="computerChoiceImg"/>
-                    <h3 ref={this.computerText} id="computerChoiceTxt">剪刀</h3>
+                    <h3 id="computerChoiceTxt">{computerText}</h3>
                     </div>
                 </div>
                 <div id="buttons">
